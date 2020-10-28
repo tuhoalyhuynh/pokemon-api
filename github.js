695ff8459e4afc641739d7a980eab81f0fa6f830
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-10-28 11:36:29 -0400

    [js] add more api functions with createElement

--- github.js
+++ github.js
@@ -1,9 +1,23 @@
+const container = document.querySelector('.container');
+
+
+
 fetch('https://api.github.com/users/tuhoalyhuynh')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData);
+    const thObj = {
+        bio: githubData.bio,
+        username: githubData.login,
+        name: githubData.name,
+    }
+    console.log(thObj);
+
+    const newElement = document.createElement('p');
+    newElement.textContent = thObj.username;
+
+    container.appendChild(newElement);
 })
 .catch(error => {
     console.log(error);
