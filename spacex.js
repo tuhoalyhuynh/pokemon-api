5e49a1adbc049484b3be236e831cb405f0d74bbf
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-10-28 11:02:38 -0400

    [project] create files, add html and js

---
+++ spacex.js
@@ -0,0 +1,43 @@
+fetch('https://api.spacexdata.com/v3/dragons')
+.then(response => {
+    return response.json();
+})
+.then(spaceXData => {
+    console.log(spaceXData);
+})
+.catch(error => {
+    console.log(error);
+})
+
+fetch('https://api.spacexdata.com/v3/dragons/dragon1')
+.then(response => {
+    return response.json();
+})
+.then(spaceXData => {
+    console.log(spaceXData);
+})
+.catch(error => {
+    console.log(error);
+})
+
+fetch('https://api.spacexdata.com/v3/payloads/TESS')
+.then(response => {
+    return response.json();
+})
+.then(spaceXData => {
+    console.log(spaceXData);
+})
+.catch(error => {
+    console.log(error);
+})
+
+fetch('https://api.spacexdata.com/v3/roadster')
+.then(response => {
+    return response.json();
+})
+.then(spaceXData => {
+    console.log(spaceXData);
+})
+.catch(error => {
+    console.log(error);
+})
